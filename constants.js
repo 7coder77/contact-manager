8ebdf1ecbc162b07dfa0390e0e9d708c4ff42094
Ani <aniruddhajoshi12@gmail.com>
2025-04-23 23:06:49 +0530

    change 2

---
+++ constants.js
@@ -0,0 +1,6 @@
+exports.constants={
+    ValidationError: 400,
+    UnauthorizedError: 401,
+    NotFoundError: 404,
+    ServerError: 500,
+}
--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -1,11 +1,45 @@
+const {constants} = require
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    
-    res.status(statusCode).json({
-        success: false,
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    });
+    switch(statusCode){
+        case constants.ValidationError:
+            res.status(400);
+            res.json({
+                title: 'Bad Request',
+                message: err.message,
+                stack: process.env.NODE_ENV === 'production' ? null : err.stack
+            });
+            break;
+        case constants.UnauthorizedError:
+            res.json({
+                title: 'Unauthorized',
+                message: err.message,
+                stack: process.env.NODE_ENV === 'production' ? null : err.stack
+            });
+            break;
+        case constants.NotFoundError:
+            res.json({
+                title: 'Not Found',
+                message: err.message,
+                stack: process.env.NODE_ENV === 'production' ? null : err.stack
+            });
+            break;
+        case constants.ServerError:
+            res.status(500);
+            res.json({
+                title: 'Server Error',
+                message: err.message,
+                stack: process.env.NODE_ENV === 'production' ? null : err.stack
+            });
+            break;
+        default:
+            console.log('No error, all good!');
+    }
+    // res.status(statusCode).json({
+    //     success: false,
+    //     message: err.message,
+    //     stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    // });
 };
 
 module.exports = errorHandler;
